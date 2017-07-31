4ab1ffd3c9cdcbad679861795b2b5a811113b9a3
kudelandrei <kudel1995@mail.ru>
2017-07-31 21:03:11 +0300

    test v2.0

--- v.2.0/dist/js/common.js
+++ v.2.0/dist/js/common.js
@@ -8,6 +8,11 @@ window.onload = function(){
 	var myReuest = new XMLHttpRequest(); // ajax запрос на получение данных работы
 	var jsonContainer = document.getElementById("works"); // контейнер, куда будут ложиться данные json
 	var btnAjax = document.getElementById("work-ajax"); // кнопка для получение json данных
+	var btnToggle = document.getElementById('toggle-menu'); // кнопка для открытия/закрытия паели с меню
+	var home = document.getElementById('home'); // для мобильников
+	var homeBefore = document.getElementById('home-before');
+	var aside = document.getElementById('aside');
+	var main = document.getElementById('main');
 	var n = 0;  // начальное количетсво выведеных элементов
 	var k = 0;  // количество выводим данных при вызове функции (проверка на избытие)
 	var URL = "../data/works.json"; 
@@ -17,20 +22,12 @@ window.onload = function(){
 		var menu = document.getElementById('menu').getElementsByTagName('li');
 		var thisMenuItem = event.target.parentNode;
 		event.preventDefault();
+		closeMobileMenu();
 		for(var i = 0; i < menu.length; i++){
 			menu[i].classList.remove('active');
 		}
 		thisMenuItem.classList.add('active');
-		//getMenuLine(event.target);
 	});
-
-	/* функция для плавающей линии */
-	// function getMenuLine(event){
-	// 	var line = document.getElementById('menu-line');
-	// 	var menuItemPos = event.getBoundingClientRect().top - 163.5;
-	// 	line.style.top = menuItemPos + 'px';
-	// 	console.log(menuItemPos);
-	// }
 
 	/* функция создания ajax зфпроса */
 	function getJson(){
@@ -40,10 +37,6 @@ window.onload = function(){
 			if (myReuest.readyState == 4 && myReuest.status == 200){
 				var myDate = JSON.parse(myReuest.response);
 				renderWorks(myDate);
-			}
-			else {
-				/* придумать как остановить запрос, если нету данных */
-				console.log(myReuest.readyState, myReuest.status);
 			}
 		}
 		myReuest.send();
@@ -63,32 +56,98 @@ window.onload = function(){
 			itemWork.querySelector('.work__date').innerHTML = dataJson[i].date;
 			itemWork.querySelector('.work__head').innerHTML = dataJson[i].head;
 			itemWork.querySelector('.work__desc').innerHTML = dataJson[i].desc;
-			for(var j = 0; j < dataJson[i].tag.lenght; j++){
-				var tagItem = document.createElement('li').className = "work__tag";
-				console.log(tagItem[j]);
-				// itemWork.querySelector('.work__tags').appendChild();
+			
+			for(var j = 0; j < dataJson[i].tag.length; j++){
+				var tags = itemWork.querySelector('.work__tags');
+				var tagItem = tags.querySelector('.work__tag');
+				tagItem.innerHTML = dataJson[i].tag[j];
+				tags.appendChild(tagItem);
 			}
-			var wrapItem = document.createElement('div');
-			wrapItem.id = 'work__item-' + i;
- 			wrapItem.appendChild(itemWork);
-			//itemWork.getElementsByClassName('work__type').innerHTML;
 
-			// var div = document.createElement("div");
-			// var workItem = div.className("work__item");
-			// workItem.innerHTML = 'sefsefsef';
-			// var image = "<div class='work__img'><img src=" + dataJson[i].img + "></img></div>";
-			// var tag = "<h3 class='work__head'>" + dataJson[i].head + "</h2>";
-			// var content = document.createElement('div');
+			itemWork.querySelector('.work__author-img').src = dataJson[i].authorImg;
+			itemWork.querySelector('.work__author-name').innerHTML = dataJson[i].authorName;
+			var wrapItem = document.createElement('div');
+ 			wrapItem.appendChild(itemWork);
 			printHTML += wrapItem.innerHTML;
 
+			if (i == dataLenght - 1) {
+				btnAjax.classList.add('disabled');
+			}
 		}
 		n += COUNT;
 
 		jsonContainer.insertAdjacentHTML('beforeend', printHTML);
 	};
 
+	/* функция открытия или закрытия меню */
+	function toogleMenu(){
+		this.classList.toggle('active');
+		isActiveMenu();
+	}
+
+	/* функция проверки меню на активность */
+	function isActiveMenu(){
+		/* меню для мобильников */ 
+		if (home.classList.contains('mobile')){
+			main.style = "min-width: 100%; left: -300px;";
+			if (btnToggle.classList.contains('active')){
+				aside.style = "left: 0px;";
+				homeBefore.style = "left: 0;";
+			}
+			else {
+				aside.style = "left: -600px;";
+			}
+		} 
+		/* меню для девайсов */
+		else {
+			homeBefore.style = "left: -100%;";
+			if (btnToggle.classList.contains('active')){
+				aside.style = "left: 0px;";
+				main.style = "min-width: auto; left: 0;";
+			}
+			else {
+				aside.style = "left: -600px;";
+				main.style = "min-width: 100%; left: -300px;";
+			}
+		}
+	}
+
+	/* функция на проверку дисплея */
+	function mobileDisplay(){
+		if(window.innerWidth < 680) {
+			home.classList.add('mobile');
+		}
+		else {
+			home.classList.remove('mobile');
+		}
+	}
+
+	/* функция для закрытия мобильного меню */ 
+	function closeMobileMenu(){
+		if (home.classList.contains('mobile')){
+			aside.style = "left: -600px;";
+			homeBefore.style = "left: -100%;";
+			btnToggle.classList.remove('active');
+			main.style = "min-width: 100%; left: -300px;";
+		}
+	}
+
+	mobileDisplay();
+
+	btnToggle.addEventListener('click', toogleMenu);
+
+	homeBefore.addEventListener('click', function(){
+		closeMobileMenu();
+	});
+
 	/* событие получения данных */
-	btnAjax.addEventListener("click", getJson); //при нажатии 
+	btnAjax.addEventListener("click", getJson); 
+
+	/* событие при изменении разришения дисплея */
+	window.onresize = function(){
+		mobileDisplay();
+		isActiveMenu();
+	};
 
 	/* событие получения данных при скролле */
 	window.onscroll = function(){
